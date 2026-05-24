7146e784cdeb47f5c51c60c1c0cd35bb857c1fc3
Fathurrahman Safari Lubis <2411500025@mahasiswa.atmaluhur.ac.id>
2026-05-24 18:05:34 +0700

    Menambahkan login session shared_preferences
    Tugas Pertemuan 9 shared preferences

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'LoginPage.dart';
+import 'DashboardPage.dart';
+import 'CounterPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PMP2411500025',
+      debugShowCheckedModeBanner: true,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/dashboard': (context) => DashboardPage(),
+        '/counter': (context) => const CounterPage(),
+      },
+    );
+  }
+}
